2668105ee03a0ba3f386d77004a8c94a650e4cb7
riesn1 <nicolasacha.riesen@students.bfh.ch>
2022-12-09 13:52:06 +0100

    added remaining factories

---
+++ database/factories/PublisherDataFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PublisherData;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PublisherDataFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PublisherData::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
