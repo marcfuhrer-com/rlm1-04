b44897258e12f1dbfffcf916d9560cbd6ab639f6
wasen2 <wasen2@students.bfh.ch>
2023-01-06 12:14:06 +0100

    P1-31 fixed css/js path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('build/assets/app.a90aeddc.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('build/assets/app.c88242eb.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
